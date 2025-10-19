c0d5e597027898cf54ccfed12eab4f2647c0b862
Vishal Koli <vishalkoli1402@gmail.com>
2025-10-19 14:28:41 +0530

    changed cors

--- src/main/java/com/notesapp/notesplatform/config/CorsConfig.java
+++ src/main/java/com/notesapp/notesplatform/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // your frontend URL
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://your-frontend-url.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
