a17f16fe4704ad60c209c1d38a9eb66ccc2052c9
Vishal Koli <vishalkoli1402@gmail.com>
2025-10-19 10:48:04 +0530

    Added .env support and updated backend config for Render deployment

--- src/main/java/com/notesapp/notesplatform/NotesplatformApplication.java
+++ src/main/java/com/notesapp/notesplatform/NotesplatformApplication.java
@@ -3,11 +3,16 @@ package com.notesapp.notesplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class NotesplatformApplication {
 
 	public static void main(String[] args) {
+		// Load .env variables into environment
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(NotesplatformApplication.class, args);
 	}
-    
+
 }
